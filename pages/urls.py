17d8541da010cd87399844e4ec2b25418eaf7001
Moisés Arrieta <moisesdavidarh@gmail.com>
2024-08-24 19:26:08 -0500

    taller 06

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from.views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView
+from.views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, CartView, CartRemoveAllView, ImageViewFactory
+from.utils import ImageLocalStorage
+#from .views import ImageViewNoDI
 
 
 urlpatterns = [
@@ -10,5 +12,12 @@ urlpatterns = [
     path('products/created/', ProductCreateView.as_view(),
         name='product-create'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+    #path('imagenodi/', ImageViewNoDI.as_view(), name='imagenodi_index'),
+    #path('image/save', ImageViewNoDI.as_view(), name='imagenodi_save'),
 ]
 
